428c343baea733a5c8df9eb318ea4f9c5677614b
Raveen Panditha <raveenpanditha9@gmail.com>
2023-10-13 00:44:52 +0530

    Created project folders

--- hello-world/pages/blogs/blogone.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function blogone() {
-  return (
-    <div>blogone</div>
-  )
-}
-
-export default blogone
--- hello-world/pages/product/sweater.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Sweaters() {
-  return (
-    <div>For sale Sweaters!</div>
-  )
-}
-
-export default Sweaters
